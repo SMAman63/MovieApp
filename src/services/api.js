5776f29cc53feb9f9d286d6beb134ce4cc6cb699
SMAman63 <aman15362177@gmail.com>
2025-06-24 14:32:58 +0530

    shaikh

---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+
+
+const API_KEY = "9da7ad8db13b1af7a142f88bacea2952";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
